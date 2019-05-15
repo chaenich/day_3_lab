d04578981c6d9b67ac77e3eb036d17d812e8cbca
Kyle Walker <Randomer44@hotmail.com>
2019-05-15 16:06:10 +0100

    MVP achieved

---
+++ my_functions.rb
@@ -0,0 +1,28 @@
+def add_array_lengths(array1, array2)
+  return array1.length + array2.length
+end
+
+# a = [1,2,3]
+# b = [3,4,5]
+#
+# p add_array_lengths(a, b)
+
+def sum_array(numbers)
+  total = 0
+  for n in numbers
+    total += n
+  end
+  return total
+end
+
+def find_item(array, str)
+  if array.include? str
+    return true
+  else
+    return false
+  end
+end
+
+def get_first_key(hash)
+  return hash.keys[0]
+end
